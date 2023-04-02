728fdbdb7cfefd6722a81f833145f8d96d73ad32
wikarsahamdany <wikarsahamdany99@gmail.com>
2023-04-03 01:23:58 +0700

    Feat : Pets-Detail & Profile

--- client-mobile/component/PetsComponent/Card.js
+++ client-mobile/component/PetsComponent/Card.js
@@ -1,29 +1,34 @@
 import { Dimensions, Image, StyleSheet, Text, View, ScrollView, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const yScreen = Dimensions.get("window").height;
 
 export default function Card() {
+  const navigation = useNavigation();
+
   return (
     <View style={{ marginVertical: 20 }}>
-      <Text style={{ fontSize: 20, fontWeight: "700", color: "#4B8CA1", marginLeft: 10 }}>Pet Clinic</Text>
+      <TouchableOpacity style={{ backgroundColor: "#e3e7fa", alignSelf: "flex-end", right: 20, borderRadius: 10 }}>
+        <Text style={{ fontSize: 20, fontWeight: "700", color: "#4B8CA1", padding: 5, paddingHorizontal: 10 }}>Add Pet</Text>
+      </TouchableOpacity>
       <ScrollView style={styles.eventMainView} showsHorizontalScrollIndicator={false}>
         <View style={{ marginVertical: 5 }}>
-          <View style={{ maxHeight: 200, display: "flex", flexDirection: "row" }}>
-            <View style={{ backgroundColor: "#e3e7fa", borderRadius: 20, minHeight: 160, minWidth: 160, justifyContent: "center", alignItems: "center" }}>
+          <TouchableOpacity style={{ maxHeight: 200, display: "flex", flexDirection: "row" }} onPress={() => navigation.navigate("petsTabDetail")}>
+            <View style={{ backgroundColor: "#e3e7fa", borderRadius: 20, borderBottomEndRadius: 10, borderTopEndRadius: 5, minHeight: 160, minWidth: 160, justifyContent: "center", alignItems: "center" }}>
               <Image style={{ width: 145, height: 145, resizeMode: "cover", borderRadius: 20 }} source={{ uri: "https://i.pinimg.com/564x/82/cc/0f/82cc0f2dae02b896a895ffe2533e2c38.jpg" }} />
             </View>
-            <View style={{ alignSelf: "center", justifyContent: "center", backgroundColor: "#e3e7fa", height: 130, width: 200, borderBottomEndRadius: 20, borderTopEndRadius: 20 }}>
+            <View style={{ alignSelf: "center", justifyContent: "center", backgroundColor: "#e3e7fa", height: 145, width: 200, borderBottomEndRadius: 20, borderTopEndRadius: 20 }}>
               <Text style={styles.titlePets}>Randy</Text>
               <Text style={styles.titlePetsDesc}>British Short Hair</Text>
             </View>
-          </View>
+          </TouchableOpacity>
         </View>
         <View style={{ marginVertical: 5 }}>
           <View style={{ maxHeight: 200, display: "flex", flexDirection: "row" }}>
-            <View style={{ backgroundColor: "#e3e7fa", borderRadius: 20, minHeight: 160, minWidth: 160, justifyContent: "center", alignItems: "center" }}>
+            <View style={{ backgroundColor: "#e3e7fa", borderRadius: 20, borderBottomEndRadius: 10, borderTopEndRadius: 5, minHeight: 160, minWidth: 160, justifyContent: "center", alignItems: "center" }}>
               <Image style={{ width: 145, height: 145, resizeMode: "cover", borderRadius: 20 }} source={{ uri: "https://i.pinimg.com/564x/3a/db/7e/3adb7eab0da04f7289ff4f469c3c96c3.jpg" }} />
             </View>
-            <View style={{ alignSelf: "center", justifyContent: "center", backgroundColor: "#e3e7fa", height: 130, width: 200, borderBottomEndRadius: 20, borderTopEndRadius: 20 }}>
+            <View style={{ alignSelf: "center", justifyContent: "center", backgroundColor: "#e3e7fa", height: 145, width: 200, borderBottomEndRadius: 20, borderTopEndRadius: 20 }}>
               <Text style={styles.titlePets}>Mail</Text>
               <Text style={styles.titlePetsDesc}>Muggle</Text>
             </View>
@@ -31,10 +36,10 @@ export default function Card() {
         </View>
         <View style={{ marginVertical: 5 }}>
           <View style={{ maxHeight: 200, display: "flex", flexDirection: "row", elevation: 10 }}>
-            <View style={{ backgroundColor: "#e3e7fa", borderRadius: 20, minHeight: 160, minWidth: 160, justifyContent: "center", alignItems: "center" }}>
+            <View style={{ backgroundColor: "#e3e7fa", borderRadius: 20, borderBottomEndRadius: 10, borderTopEndRadius: 5, minHeight: 160, minWidth: 160, justifyContent: "center", alignItems: "center" }}>
               <Image style={{ width: 145, height: 145, resizeMode: "cover", borderRadius: 20 }} source={{ uri: "https://i.pinimg.com/564x/4a/bb/8d/4abb8d32d097e7fd2e1643039e06a60a.jpg" }} />
             </View>
-            <View style={{ alignSelf: "center", justifyContent: "center", backgroundColor: "#e3e7fa", height: 130, width: 200, borderBottomEndRadius: 20, borderTopEndRadius: 20 }}>
+            <View style={{ alignSelf: "center", justifyContent: "center", backgroundColor: "#e3e7fa", height: 145, width: 200, borderBottomEndRadius: 20, borderTopEndRadius: 20 }}>
               <Text style={styles.titlePets}>Zio</Text>
               <Text style={styles.titlePetsDesc}>Super Paw</Text>
             </View>
